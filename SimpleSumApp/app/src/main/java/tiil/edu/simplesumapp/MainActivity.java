4798a214a3b43dbaba98dffdfab6d7500e5a5299
luong141102 <ngocluong3959@gmail.com>
2024-03-03 12:59:11 +0700

    simpleSum2
    cvb

--- SimpleSumApp/app/src/main/java/tiil/edu/simplesumapp/MainActivity.java
+++ SimpleSumApp/app/src/main/java/tiil/edu/simplesumapp/MainActivity.java
@@ -3,6 +3,8 @@ package tiil.edu.simplesumapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void XuLyCong(View view){
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKQ);
+
+        String strA = editTextSoA.getText().toString();
+        String strB = editTextSoB.getText().toString();
+
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+        editTextKQ.setText(strTong);
+    }
+    public void Reset(View view){
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKQ = findViewById(R.id.edtKQ);
+        editTextSoA.setText("");
+        editTextSoB.setText("");
+        editTextKQ.setText("");
     }
 }
