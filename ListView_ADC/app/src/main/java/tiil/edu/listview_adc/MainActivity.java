760383503f97da8920ca4c7c9d5908efaedcf446
luong141102 <ngocluong3959@gmail.com>
2024-03-13 16:18:11 +0700

    vd
    fgb

---
+++ ListView_ADC/app/src/main/java/tiil/edu/listview_adc/MainActivity.java
@@ -0,0 +1,14 @@
+package tiil.edu.listview_adc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
