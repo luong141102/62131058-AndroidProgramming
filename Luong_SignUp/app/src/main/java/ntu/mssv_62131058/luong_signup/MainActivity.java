8b1eabac8587f1dea0243d0ec2402b42d9bd095b
luong141102 <ngocluong3959@gmail.com>
2024-04-06 11:51:09 +0700

    ok
    jgf

--- Luong_SignUp/app/src/main/java/ntu/mssv_62131058/luong_signup/MainActivity.java
+++ Luong_SignUp/app/src/main/java/ntu/mssv_62131058/luong_signup/MainActivity.java
@@ -2,13 +2,40 @@ package ntu.mssv_62131058.luong_signup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLogout;
+    FirebaseAuth auth;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogout = findViewById(R.id.btnLogout);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        if (user == null){
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }else {
+
+        }
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
